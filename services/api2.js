a7348ac8d3070cd0af1b68fe6bd6a5d14e532045
Alexandre_oliveiradev <125042006+Alexandretecsomobi@users.noreply.github.com>
2023-04-19 20:28:51 -0300

    Delete services directory

--- services/api2.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-const api = axios.create({
-    baseURL: "https://web-production-a074.up.railway.app/",
-    //baseURL: "https://apivittoriaviitstore.herokuapp.com/",
-});
-
-export default api;
